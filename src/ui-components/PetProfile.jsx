1ee0dc3ef16cbfb3366bacb9d648e9a9cdaa22ab
Souresh Coumar <soureshcoumar@gmail.com>
2022-08-08 20:49:12 +0530

    working application

---
+++ src/ui-components/PetProfile.jsx
@@ -0,0 +1,97 @@
+/***************************************************************************
+ * The contents of this file were generated with Amplify Studio.           *
+ * Please refrain from making any modifications to this file.              *
+ * Any changes to this file will be overwritten when running amplify pull. *
+ **************************************************************************/
+
+/* eslint-disable */
+import React from "react";
+import { getOverrideProps } from "@aws-amplify/ui-react/internal";
+import { Button, Flex, Image, Text } from "@aws-amplify/ui-react";
+export default function PetProfile(props) {
+  const { pet, overrides, ...rest } = props;
+  return (
+    <Flex
+      gap="24px"
+      direction="column"
+      width="320px"
+      alignItems="center"
+      position="relative"
+      padding="24px 24px 24px 24px"
+      backgroundColor="rgba(255,255,255,1)"
+      {...rest}
+      {...getOverrideProps(overrides, "PetProfile")}
+    >
+      <Image
+        width="160px"
+        height="160px"
+        shrink="0"
+        position="relative"
+        borderRadius="160px"
+        padding="0px 0px 0px 0px"
+        src={pet?.image}
+        {...getOverrideProps(overrides, "image")}
+      ></Image>
+      <Flex
+        gap="8px"
+        direction="column"
+        alignItems="center"
+        shrink="0"
+        position="relative"
+        padding="0px 0px 0px 0px"
+        {...getOverrideProps(overrides, "Name")}
+      >
+        <Text
+          fontFamily="Inter"
+          fontSize="20px"
+          fontWeight="700"
+          color="rgba(13,26,38,1)"
+          lineHeight="25px"
+          textAlign="center"
+          display="flex"
+          direction="column"
+          justifyContent="flex-start"
+          shrink="0"
+          position="relative"
+          padding="0px 0px 0px 0px"
+          whiteSpace="pre-wrap"
+          children={pet?.name}
+          {...getOverrideProps(overrides, "Melinda Marcus")}
+        ></Text>
+        <Text
+          fontFamily="Inter"
+          fontSize="16px"
+          fontWeight="400"
+          color="rgba(48,64,80,1)"
+          lineHeight="24px"
+          textAlign="center"
+          display="flex"
+          direction="column"
+          justifyContent="flex-start"
+          letterSpacing="0.01px"
+          shrink="0"
+          position="relative"
+          padding="0px 0px 0px 0px"
+          whiteSpace="pre-wrap"
+          children={pet?.about}
+          {...getOverrideProps(overrides, "Design Engineer at Cloth Studios")}
+        ></Text>
+      </Flex>
+      <Button
+        display="flex"
+        gap="0"
+        justifyContent="center"
+        alignItems="center"
+        shrink="0"
+        alignSelf="stretch"
+        objectFit="cover"
+        position="relative"
+        size="large"
+        isDisabled={false}
+        variation="primary"
+        children="View Profile"
+        {...getOverrideProps(overrides, "Button")}
+      ></Button>
+    </Flex>
+  );
+}
